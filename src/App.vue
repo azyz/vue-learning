dcdc697188024ecd7198fea5c79f500f1896be2c
azyz <1411232058@qq.com>
2023-07-29 22:05:51 +0800

    路由设计和配置

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script setup>
 import { useUserStroe } from '@/stores/pinia'
-
 const userStore = useUserStroe()
+
 </script>
 
 <template>
-  <div>我是app</div>
+  <div>
+    <hr />
+    <router-view></router-view>
+    <hr />
+    <hr />
+    <hr />
+
+    我是app
+  </div>
   <el-button>按钮</el-button>
   <p>{{ userStore.token }}</p>
+
   <el-button @click="userStore.setToken('aadvsdad')">登录</el-button>
   <el-button @click="userStore.removeToken()">退出</el-button>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,36 @@ import { createRouter, createWebHistory } from 'vue-router'
 //vite中的环境变量  import.meta.env.BASE_URL  可在vite.config.js中加base配置前缀
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    { path: '/login', component: () => import('@/views/login/loginPage.vue') }, //登录页
+    {
+      path: '/',
+      component: () => import('@/views/layout/LayoutContainer.vue'),
+      redirect: '/article/manage',
+      children: [
+        {
+          path: '/article/manage',
+          component: () => import('@/views/article/ArticleManage.vue')
+        },
+        {
+          path: '/article/channel',
+          component: () => import('@/views/article/ArticleChannel.vue')
+        },
+        {
+          path: '/user/avatar',
+          component: () => import('@/views/user/UserAvatar.vue')
+        },
+        {
+          path: '/user/password',
+          component: () => import('@/views/user/UserPassword.vue')
+        },
+        {
+          path: '/user/profile',
+          component: () => import('@/views/user/UserProfile.vue')
+        }
+      ]
+    }
+  ]
 })
 
 export default router
